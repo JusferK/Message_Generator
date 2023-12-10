76baa7488a68e1ba2444c72cbeaf84bad3204875
JusferK <fer123arriaga@gmail.com>
2023-12-10 14:16:28 -0600

    stored quotes in const

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// all quotes will be choosen from here.
+const senecaQ = [
+    "We suffer more often in imagination than in reality.",
+    "Ignorance is the cause of fear.",
+    "While we wait for life, life passes.",
+    "Life is long, if you know how to use it",
+    "Hurry up and live.",
+    "ease to hope and you will cease to fear.",
+    "Wealth is the slave of a wise man and the master of a fool.",
+    "Only time can heal what reason cannot.",
+    "While we are postponing, life speeds by.",
+    "Life, if well lived, is long enough."
+];
+
+const marcusQ = [
+    "The best answer to anger is silence.",
+    "The more we value things outside our control, the less control we have.",
+    "How ridiculous and how strange to be surprised at anything which happens in life.",
+    "You have power over your mind – not outside events. Realize this, and you will find strength.",
+    "The best revenge is not to be like your enemy.",
+    "Accept the things to which fate binds you, and love the people with whom fate brings you together, but do so with all your heart.",
+    "Confine yourself to the present.",
+    "When you arise in the morning, think of what a precious privilege it is to be alive – to breathe, to think, to enjoy, to love.",
+    "You can commit injustice by doing nothing.",
+    "Waste no more time arguing about what a good man should be. Be one."
+];
+
+const epictetusQ = [
+    "We have two ears and one mouth so that we can listen twice as much as we speak.",
+    "No man is free who is not master of himself.",
+    "Man are disturbed not by things, but by the view which they take them.",
+    "It is difficulties that show what men are.",
+    "Make the best use of what is in your power, and take the rest as it happens.",
+    "A ship should not ride on a single anchor, nor life on a single hope.",
+    "No great thing is created suddenly.",
+    "Know, first, who you are, and then adorn yourself accordingly.",
+    "It is the nature of the wise to resist pleasures, but the foolish to be a slave to them.",
+    "First learn the meaning of what you say, and then speak.",
+    "If you want something good, get it yourself."
+];
+
+const zenoQ = [
+    "All the good are friends of one another.",
+    "No loss should be more regrettable to us than losing our time, for it’s irretrievable.",
+    "Extravagance is its own destroyer",
+    "Man conquers the world by conquering himself.",
+    "Steel your sensibilities, so that life shall hurt you as little as possible.",
+    "Better to trip with the feet than with the tongue",
+    "A bad feeling is a commotion of the mind repugnant to reason, and against nature."
+];
+
+const musoniusQ = [
+    "We begin to lose our hesitation to do immoral things when we lose our hesitation to speak of them.",
+    "Humanity must seek what is NOT simple and obvious using the simple and obvious.",
+    "Since every man dies, it is better to die with distinction than to live long.",
+    "Only by exhibiting actions in harmony with the sound words which he has received will anyone be helped by philosophy.",
+    "Thus whoever destroys human marriage destroys the home, the city-the whole human race.",
+];
+
+const diogenesQ = [
+    "He with the most who is content with the least.",
+    "The foundation of every state is the education of its youth.",
+    "Blushing is the color of virtue.",
+    "Dogs and philosophers do the greatest good and get the fewest rewards.",
+    "I am not an Athenian or a Greek, but a citizen of the world.",
+    "The mob is the mother of tyrants.",
+    "Poverty is a virtue which one can teach oneself."
+];
+
